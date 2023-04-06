6c7ddecceabf9da9902531d1f00a35e87ada4b0b
Md Antonin Islam <antonin.islam.dev@gmail.com>
2023-04-06 14:55:48 +0600

    `Adds` receipe modal

---
+++ src/app/recipes/recipe.modal.ts
@@ -0,0 +1,13 @@
+class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, desc: string, image: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = image;
+    }
+}
+
+export { Recipe };
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.modal';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  styleUrls: ['./recipes.component.css'],
 })
-export class RecipesComponent {
-
+export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://picsum.photos/id/1/200/300'
+    ),
+  ];
+  ngOnInit(): void {}
 }
