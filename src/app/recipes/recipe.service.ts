8e5cf692e5db4849fced34ea65f9e6a798ad0761
Md Antonin Islam <antonin.islamd.dev@gmail.com>
2023-04-11 11:00:30 +0600

    `Adds` recipe function
    - adds json server
    - adds recipe fetch
    - adds recipe add
    - adds reactive form

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,35 @@
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.modal';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
 class RecipeService {
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://picsum.photos/id/1/200/300'
-    ),
-  ];
+  private recipes: any = [];
+
+  constructor(private http: HttpClient) {}
 
   getRecipe() {
     return this.recipes;
   }
 
-  addRecipe(recipe: Recipe){
+  fetchRecipe() {
+    this.http.get('http://localhost:3000/recipes').subscribe((res: any) => {
+      // console.log(res);
+      res.forEach((recipe) => {
+        this.addRecipeToState(recipe);
+      });
+    });
+  }
+
+  addRecipeToState(recipe: Recipe) {
     this.recipes.push(recipe);
+  }
+
+  addRecipe(recipe: Recipe){
+    this.http.post('http://localhost:3000/recipes', recipe).subscribe((res: any) => {
+      console.log(res);
+      this.addRecipeToState(recipe);
+    })
   }
 }
 
